1ff7ab49e5ad0c26eae6cd8f3dd307651dc10f2c
Camilo III P. Ortillo <camortillo@gmail.com>
2020-03-03 18:11:20 -0500

    renamed node files for clarity, added driver file for problem #5

---
+++ project-1-trees/construct.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.insert(1, './BBST')
+sys.path.insert(2, './BST')
+
+import BBST
+import BST
+
+from arrays import getRandomArray
+
+input = getRandomArray(10000)
+
+bbst = None
+bst = None
+for i in input:
+  pass
